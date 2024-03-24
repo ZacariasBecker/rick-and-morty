8d3d2ed3c141dde4fab087d3c6a0287c3ba27fae
ZacariasBecker <zacariasbecker@gmail.com>
2024-03-24 18:09:26 -0300

    detail cards

--- src/app/episode-details/episode-details.component.ts
+++ src/app/episode-details/episode-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RMapiService } from '../rmapi.service';
-import { ActivatedRoute } from '@angular/router';
-import { ResultsEpisodeInterface } from '../results-interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResultsCharacterInterface, ResultsEpisodeInterface } from '../results-interface';
+import { CommonModule } from '@angular/common';
+import { CharacterCardComponent } from '../character-card/character-card.component';
 
 @Component({
   selector: 'app-episode-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CharacterCardComponent],
   templateUrl: './episode-details.component.html',
   styleUrl: './episode-details.component.css'
 })
@@ -16,10 +18,26 @@ export class EpisodeDetailsComponent {
 
   dataEpisode?: ResultsEpisodeInterface;
 
-  constructor() {
-    const currentCharactersId = parseInt(this.route.snapshot.params['id'] || 1);
-    this.rmapiService.getADetailDataByID<ResultsEpisodeInterface>('episode', currentCharactersId).then((dataCharacter: ResultsEpisodeInterface) => {
-      this.dataEpisode = dataCharacter;
+  characters?: ResultsCharacterInterface[] = [];
+
+  navigateToLocation = async (url?: string) => {
+    console.log(url?.split('character/')[1]);
+    await this.router.navigate(['character', url?.split('character/')[1]]);
+  };
+
+  constructor(private router: Router) {
+    const currentEpisodesId = parseInt(this.route.snapshot.params['id'] || 1);
+    this.rmapiService.getADetailDataByID<ResultsEpisodeInterface>('episode', currentEpisodesId).then((dataEpisode: ResultsEpisodeInterface) => {
+      this.dataEpisode = dataEpisode;
+
+      let characters = '';
+      this.dataEpisode.characters.forEach(character => characters += (character.split('character/')[1] + ','));
+
+      this.rmapiService.getADetailDataByID<ResultsCharacterInterface[]>('character', characters).then((characters: ResultsCharacterInterface[]) => {
+        this.characters = characters;
+      });
+
     });
   }
+
 }
--- src/app/location-details/location-details.component.ts
+++ src/app/location-details/location-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RMapiService } from '../rmapi.service';
-import { ActivatedRoute } from '@angular/router';
-import { ResultsLocationInterface } from '../results-interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResultsCharacterInterface, ResultsLocationInterface } from '../results-interface';
+import { CommonModule } from '@angular/common';
+import { CharacterCardComponent } from '../character-card/character-card.component';
 
 @Component({
   selector: 'app-location-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CharacterCardComponent],
   templateUrl: './location-details.component.html',
   styleUrl: './location-details.component.css'
 })
@@ -16,10 +18,26 @@ export class LocationDetailsComponent {
 
   dataLocation?: ResultsLocationInterface;
 
-  constructor() {
+  residents?: ResultsCharacterInterface[] = [];
+
+  navigateToLocation = async (url?: string) => {
+    console.log(url?.split('character/')[1]);
+    await this.router.navigate(['character', url?.split('character/')[1]]);
+  };
+
+  constructor(private router: Router) {
     const currentCharactersId = parseInt(this.route.snapshot.params['id'] || 1);
     this.rmapiService.getADetailDataByID<ResultsLocationInterface>('location', currentCharactersId).then((dataLocation: ResultsLocationInterface) => {
       this.dataLocation = dataLocation;
+
+      let residents = '';
+      this.dataLocation.residents.forEach(resident => residents += (resident.split('character/')[1] + ','));
+
+      this.rmapiService.getADetailDataByID<ResultsCharacterInterface[]>('character', residents).then((residents: ResultsCharacterInterface[]) => {
+        this.residents = residents;
+      });
+
     });
   }
+
 }
